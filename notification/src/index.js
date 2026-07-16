2e4530da7429ed6f0c3e4323e116d60682230cc3
Abhishekanand19 <gudubro2005@gmail.com>
2026-07-16 23:15:16 +0530

    feat: add individual file notification/src/index.js

---
+++ notification/src/index.js
@@ -0,0 +1,11 @@
+// PHASE 2: `import "./tracing.js";` goes on the line ABOVE everything else.
+import { createApp } from "./app.js";
+import { config } from "./config.js";
+
+const server = createApp().listen(config.port, () => {
+  console.log(`[${config.serviceName}] listening on :${config.port}`);
+});
+
+for (const sig of ["SIGINT", "SIGTERM"]) {
+  process.on(sig, () => server.close(() => process.exit(0)));
+}
