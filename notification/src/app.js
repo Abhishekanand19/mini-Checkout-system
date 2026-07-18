6115bbbd6f71a4adcf03ff4dd0c5bff3d33cbf3f
Abhishekanand19 <gudubro2005@gmail.com>
2026-07-18 08:21:30 +0530

    refactor(notification): update application core setup

--- notification/src/app.js
+++ notification/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { requestContext } from "./middleware/requestContext.js";
 import { notificationRoutes } from "./routes/notification.routes.js";
 import { config } from "./config.js";
+import { logger } from "./logger.js";
 
 export function createApp() {
   const app = express();
@@ -18,7 +19,10 @@ export function createApp() {
 
   // eslint-disable-next-line no-unused-vars
   app.use((err, req, res, next) => {
-    console.error(`[${config.serviceName}] unhandled`, err);
+    logger.error(
+      { err: err.message, request_id: req.context?.requestId },
+      "unhandled error",
+    );
     res.status(500).json({ error: "INTERNAL_ERROR" });
   });
 
