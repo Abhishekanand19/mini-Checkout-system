576b0a172bb2e2269135f76710a377031ada1344
Abhishekanand19 <gudubro2005@gmail.com>
2026-07-18 08:21:18 +0530

    chore(gateway): update entry point script

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -1,9 +1,10 @@
 import { createApp } from "./app.js";
 import { config } from "./config.js";
+import { logger } from "./logger.js";
 import { pool } from "./db.js";
 
 const server = createApp().listen(config.port, () => {
-  console.log(`[${config.serviceName}] listening on :${config.port}`);
+  logger.info({ port: config.port }, "service started");
 });
 
 for (const sig of ["SIGINT", "SIGTERM"]) {
